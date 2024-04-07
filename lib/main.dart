a4333cd26c2ab8d4a5384035d6f8269ff72e4e97
Mina Magdy <74681314+mina-nasralla@users.noreply.github.com>
2024-04-08 00:38:35 +0200

    setup Gitit Service Locaator completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:booklyapp/Core/utilites/Service_locator/service.dart';
+import 'package:booklyapp/Features/Home/data/Repos/Home%20_Repo_Impl.dart';
+import 'package:booklyapp/Features/Home/presentation/Manger/Featured_books_cubit/featured_books_cubit.dart';
+import 'package:booklyapp/Features/Home/presentation/Manger/Newset_Books_Cubit/newset_books_cubit.dart';
 import 'package:booklyapp/Features/splach/presentation/views/splash_screen.dart';
 import 'package:booklyapp/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupGititLocaator();
   runApp(const Bookly());
 }
 
@@ -13,17 +19,23 @@ class Bookly extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Bookly App',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Kprimarycolor,
-        textTheme: GoogleFonts.montserratTextTheme(
-          ThemeData.dark().textTheme,
-        )
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoEmpl>())),
+        BlocProvider(
+            create: (context) => NewsetBooksCubit(getIt.get<HomeRepoEmpl>()))
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Bookly App',
+        theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: Kprimarycolor,
+            textTheme: GoogleFonts.montserratTextTheme(
+              ThemeData.dark().textTheme,
+            )),
+        home: const SplashScreen(),
       ),
-      home: const SplashScreen(),
     );
   }
-
 }
